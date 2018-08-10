d58386c6ab5b69a48872074933e6268616e7ec57
Romain Beguet <beguet@adacore.com>
2018-08-10 16:43:18 +0200

    R806-012: Add --smart-conditional-filter switch.
    Change-Id: Ic9dcab895fd9ef1afa3eae34fee2b1d7406f8386

--- lalcheck/checkers/duplicate_branches.py
+++ lalcheck/checkers/duplicate_branches.py
@@ -40,7 +40,7 @@ class Results(SyntacticChecker.Results):
         fst_line = diag[0].sloc_range.start.line
         return (
             DiagnosticPosition.from_node(diag[1]),
-            'duplicate code with line {}'.format(fst_line),
+            'duplicate code with line {}'.format(fst_line) + diag[2],
             DuplicateCode,
             cls.HIGH
         )
@@ -50,9 +50,13 @@ CheckerConfig = namedtuple('CheckerConfig', (
     'size_threshold',
     'newline_factor',
     'min_duplicates',
+    'smart_conditional_filter',
     'do_ifs',
     'do_cases'
 ))
+
+
+_additional_msg_format = " (did you mean to use {}?)"
 
 
 def find_duplicate_branches(config, unit):
@@ -119,57 +123,162 @@ def find_duplicate_branches(config, unit):
             """
             if isinstance(node, lal.IfStmt):
                 if len(node.f_alternatives) == 0:
-                    return node.f_then_stmts
+                    return node.f_cond_expr, node.f_then_stmts
                 else:
-                    return node.f_alternatives[-1].f_stmts
+                    return (
+                        node.f_alternatives[-1].f_cond_expr,
+                        node.f_alternatives[-1].f_stmts
+                    )
             else:
                 if len(node.f_alternatives) == 0:
-                    return node.f_then_expr
+                    return node.f_cond_expr, node.f_then_expr
                 else:
-                    return node.f_alternatives[-1].f_then_expr
+                    return (
+                        node.f_alternatives[-1].f_cond_expr,
+                        node.f_alternatives[-1].f_then_expr
+                    )
 
         if isinstance(node, lal.IfStmt) and config.do_ifs:
             blocks = []
             if select_if_block(node.f_then_stmts, node.f_cond_expr):
-                blocks += [node.f_then_stmts]
-            blocks += [sub.f_stmts for sub in node.f_alternatives
+                blocks += [(node.f_cond_expr, node.f_then_stmts)]
+
+            blocks += [(sub.f_cond_expr, sub.f_stmts)
+                       for sub in node.f_alternatives
                        if select_if_block(sub.f_stmts, sub.f_cond_expr)]
+
             # Only return the else block if it is the same as the block
             # preceding it. Otherwise, there may be valid reasons for code
             # duplication, that have to do with the order of evaluation of
             # tests in an if-statement.
-            if (node.f_else_stmts and
-                    have_same_tokens(node.f_else_stmts,
-                                     last_block_before_else(node))):
-                blocks += [node.f_else_stmts]
+            if node.f_else_stmts:
+                last_cond, last_block = last_block_before_else(node)
+                if have_same_tokens(node.f_else_stmts, last_block):
+                    blocks += [(last_cond, node.f_else_stmts)]
 
         elif isinstance(node, lal.IfExpr) and config.do_ifs:
             blocks = []
             if select_if_block(node.f_then_expr, node.f_cond_expr):
-                blocks += [node.f_then_expr]
-            blocks += [sub.f_then_expr for sub in node.f_alternatives
+                blocks += [(node.f_cond_expr, node.f_then_expr)]
+            blocks += [(sub.f_cond_expr, sub.f_then_expr)
+                       for sub in node.f_alternatives
                        if select_if_block(sub.f_then_expr, sub.f_cond_expr)]
+
             # Only return the else block if it is the same as the block
             # preceding it. Otherwise, there may be valid reasons for code
             # duplication, that have to do with the order of evaluation of
             # tests in an if-expression.
-            if (node.f_else_expr and
-                    have_same_tokens(node.f_else_expr,
-                                     last_block_before_else(node))):
-                blocks += [node.f_else_expr]
+            if node.f_else_expr:
+                last_cond, last_expr = last_block_before_else(node)
+                if have_same_tokens(node.f_else_expr, last_expr):
+                    blocks += [(last_cond, node.f_else_expr)]
 
         elif isinstance(node, lal.CaseStmt) and config.do_cases:
-            blocks = [sub.f_stmts for sub in node.f_alternatives
+            blocks = [(sub.f_choices, sub.f_stmts)
+                      for sub in node.f_alternatives
                       if select_case_block(sub.f_stmts)]
 
         elif isinstance(node, lal.CaseExpr) and config.do_cases:
-            blocks = [sub.f_expr for sub in node.f_cases
+            blocks = [(sub.f_choices, sub.f_expr)
+                      for sub in node.f_cases
                       if select_case_block(sub.f_expr)]
 
         else:
             blocks = []
 
         return blocks
+
+    def is_interesting_element(node):
+        if node.is_a(lal.Identifier):
+            # Todo : only object decls, enum lits?
+            return True
+
+        return False
+
+    def interesting_elements_in(node):
+        return frozenset(x.text for x in node.findall(is_interesting_element))
+
+    def find_duplicates_with_smart_filter(all_blocks):
+        duplicates = []
+
+        seen_blocks = {}
+        for cond, block in all_blocks:
+            # Find interesting elements in the condition that leads to this
+            # block.
+            cond_interests = interesting_elements_in(cond)
+
+            block_tokens = tokens_info(block)
+            block_info = seen_blocks.get(block_tokens)
+
+            if block_info is not None:  # If we have seen this block already
+                # Retrieve the interesting elements in the condition that lead
+                # to the original block.
+                orig_cond_interests, block_interests, orig_block = block_info
+
+                # Compute the symmetric difference between the two sets of
+                # interesting elements (get those that are in one of them but
+                # not in both).
+                interests_diff = orig_cond_interests.symmetric_difference(
+                    cond_interests
+                )
+
+                # Try to find in the block itself occurrences of the
+                # interesting elements that appear only in one of the
+                # conditions.
+                block_occurrences = frozenset(
+                    e
+                    for e in block_interests
+                    if e in interests_diff
+                )
+
+                # This may mean that one of the two blocks was not updated
+                # properly, so register it.
+                if len(block_occurrences) > 0:
+                    # Compute the set of interesting elements that appear
+                    # in one of the condition but not in the duplicated block.
+                    unused_elems = interests_diff - block_occurrences
+                    if len(unused_elems) == 1:
+                        # If there is only one, maybe the user forgot to use
+                        # that variable in the block where this element appears
+                        # in the condition.
+
+                        # Retrieve the sole element and create the message.
+                        unused_elem = next(iter(unused_elems))
+                        msg = _additional_msg_format.format(unused_elem)
+
+                        if unused_elem in orig_cond_interests:
+                            # If the unused element appears in the condition
+                            # of the first block, that means the first block
+                            # is probably the duplicate.
+                            duplicates.append((block, orig_block, msg))
+                        else:
+                            # If the unused element appears in the condition
+                            # of the second block, that means the second block
+                            # is probably the duplicate.
+                            duplicates.append((orig_block, block, msg))
+                    else:
+                        duplicates.append((orig_block, block, ""))
+            else:
+                seen_blocks[block_tokens] = (
+                    cond_interests,
+                    interesting_elements_in(block),
+                    block
+                )
+
+        return duplicates
+
+    def find_duplicates_without_smart_filter(all_blocks):
+        duplicates = []
+
+        seen_blocks = {}
+        for _, block in all_blocks:
+            tokens = tokens_info(block)
+            if tokens in seen_blocks:
+                duplicates.append((seen_blocks[tokens], block, ""))
+            else:
+                seen_blocks[tokens] = block
+
+        return duplicates
 
     def has_same_blocks(node):
         """
@@ -180,14 +289,11 @@ def find_duplicate_branches(config, unit):
         :rtype: lal.Expr|None
         """
         all_blocks = list_blocks(node)
-        tokens_to_block = {}
-        duplicates = []
-        for block in all_blocks:
-            tokens = tokens_info(block)
-            if tokens in tokens_to_block:
-                duplicates.append((tokens_to_block[tokens], block))
-            else:
-                tokens_to_block[tokens] = block
+
+        if config.smart_conditional_filter:
+            duplicates = find_duplicates_with_smart_filter(all_blocks)
+        else:
+            duplicates = find_duplicates_without_smart_filter(all_blocks)
 
         min_duplicates = (len(all_blocks) if config.min_duplicates == -1
                           else config.min_duplicates)
@@ -265,6 +371,7 @@ class DuplicateBranchesChecker(SyntacticChecker):
                 size_threshold=arg_values.size_threshold,
                 newline_factor=arg_values.newline_factor,
                 min_duplicates=arg_values.min_duplicates,
+                smart_conditional_filter=arg_values.smart_conditional_filter,
                 do_ifs=not arg_values.only_cases,
                 do_cases=not arg_values.only_ifs
             )
@@ -291,6 +398,14 @@ class DuplicateBranchesChecker(SyntacticChecker):
                                  "branches in the if/case statement are "
                                  "considered duplicates. Use -1 to report "
                                  "only when ALL branches are duplicates.")
+
+        parser.add_argument('--smart-conditional-filter', action='store_true',
+                            help="Check if an element (variable, etc.) of a "
+                                 "duplicated code appears in only one of the "
+                                 "two guarding conditions that leads to the "
+                                 "duplicated code, suggesting the user may "
+                                 "have forgotten to update one of the two "
+                                 "blocks.")
 
         group = parser.add_mutually_exclusive_group(required=False)
         group.add_argument('--only-ifs', action='store_true',
