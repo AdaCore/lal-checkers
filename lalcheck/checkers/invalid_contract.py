2b7b42a0ff00fc416fea764fa55d7f341a3e71c1
Romain Beguet <beguet@adacore.com>
2018-11-02 18:31:30 +0100

    R920-075: Discard predetermined tests msgs for statically known exprs.
    Change-Id: I3e6eb57f102df916cff99a60b91636253f1335c1

--- lalcheck/checkers/invalid_contract.py
+++ lalcheck/checkers/invalid_contract.py
@@ -12,7 +12,8 @@ from lalcheck.checkers.support.checker import (
 from lalcheck.checkers.support.components import AbstractSemantics
 from lalcheck.checkers.support.kinds import ContractCheck as KindContractCheck
 from lalcheck.checkers.support.utils import (
-    collect_assumes_with_purpose, orig_text_matches, eval_expr_at
+    collect_assumes_with_purpose, orig_bool_expr_statically_equals,
+    eval_expr_at
 )
 from lalcheck.tools import dot_printer
 from lalcheck.tools.digraph import Digraph
@@ -136,13 +137,12 @@ def find_violated_contracts(analysis):
     contract_checks = collect_assumes_with_purpose(analysis.cfg, ContractCheck)
 
     # Use the semantic analysis to evaluate at those program points the
-    # contract expressions. Filter out contracts explicitly set to False by
-    # users.
+    # contract expressions. Filter out contracts which are trivially false.
     contract_check_values = [
         (trace, purpose, value)
         for node, check_expr, purpose in contract_checks
         for trace, value in eval_expr_at(analysis, node, check_expr)
-        if not orig_text_matches(check_expr, ('false',))
+        if not orig_bool_expr_statically_equals(check_expr, (False,))
     ]
 
     # Finally, keep those that might fail.
